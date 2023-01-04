2c4cd3e60c6639c0667eab20829c18cbd4b6f2bc
Nguyen <quangtri.nguyen@dxc.com>
2023-01-04 17:04:47 +0700

    Search for Products by Category

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,15 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 import { ProductService } from './services/product.service';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+    {path: 'category/:id', component: ProductListComponent},
+    {path: 'category', component: ProductListComponent},
+    {path: 'products', component: ProductListComponent},
+    {path: '', redirectTo: '/products', pathMatch: 'full'},
+    {path: '**', redirectTo: '/products', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -11,6 +20,7 @@ import { ProductService } from './services/product.service';
     ProductListComponent
   ],
   imports: [
+    RouterModule.forRoot(routes),
     BrowserModule,
     HttpClientModule
   ],
