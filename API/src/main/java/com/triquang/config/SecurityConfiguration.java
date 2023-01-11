c308c054666a3472aedd491807dd9eb8bec83ba3
Nguyen <quangtri.nguyen@dxc.com>
2023-01-11 16:41:53 +0700

    Security - Order History

---
+++ API/src/main/java/com/triquang/config/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.triquang.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.accept.ContentNegotiationStrategy;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+
+import com.okta.spring.boot.oauth.Okta;
+
+@Configuration
+public class SecurityConfiguration {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http.authorizeRequests(configurer -> 
+					configurer.antMatchers("/api/orders/**")
+							   .authenticated())
+		.oauth2ResourceServer()
+		.jwt();
+		http.cors();
+		
+		// Add content negotiation strategy
+		http.setSharedObject(ContentNegotiationStrategy.class, new HeaderContentNegotiationStrategy());
+		
+		Okta.configureResourceServer401ResponseBody(http);
+		
+		return http.build();
+		
+	}
+}
